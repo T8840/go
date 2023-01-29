4573df64ffa7b813cbf0390179553635268614ff
T8840 <673683677@qq.com>
2023-01-29 21:17:00 +0800

    add "并发模式"

---
+++ internal/worker/worker.go
@@ -0,0 +1,48 @@
+// Example 使用无缓冲的通道来创建一个goroutine池，这些goroutine执行并控制一组工作，让其并发执行.
+// Package work manages a pool of goroutines to perform work.
+package worker
+
+import "sync"
+
+// Worker must be implemented by types that want to use
+// the work pool.
+type Worker interface {
+	Task()
+}
+
+// Pool provides a pool of goroutines that can execute any Worker
+// tasks that are submitted.
+type Pool struct {
+	work chan Worker
+	wg   sync.WaitGroup
+}
+
+// New creates a new work pool.
+func New(maxGoroutines int) *Pool {
+	p := Pool{
+		work: make(chan Worker),
+	}
+
+	p.wg.Add(maxGoroutines)
+	for i := 0; i < maxGoroutines; i++ {
+		go func() {
+			for w := range p.work {
+				w.Task()
+			}
+			p.wg.Done()
+		}()
+	}
+
+	return &p
+}
+
+// Run submits work to the pool.
+func (p *Pool) Run(w Worker) {
+	p.work <- w
+}
+
+// Shutdown waits for all the goroutines to shutdown.
+func (p *Pool) Shutdown() {
+	close(p.work)
+	p.wg.Wait()
+}
